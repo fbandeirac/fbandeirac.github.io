e4dea5fa6879cdd03dde1a08ed0e9e198986abb2
Felipe Bandeira <fbandeirac@gmail.com>
2020-08-01 19:49:28 -0300

    Updated Icons, Skills and Languages.

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5d4ad4d79d30fb361054.js"
+    "url": "webpack-runtime-3ebfefadf483ab57df78.js"
   },
   {
     "url": "framework-17193c917176a9ab3be2.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "532a2f07-11072610f417286cefd7.js"
   },
   {
-    "url": "app-f7009dad99e39c476379.js"
+    "url": "app-905b0ae7eb9f484b531e.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bfa79a0314aced1aa13b28b589831714"
+    "revision": "884ff06189c5c171d03c1677fe898fc8"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
@@ -50,7 +50,7 @@ self.__precacheManifest = [
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "c1c87c92afa33fa461a13ddd20990053"
+    "revision": "b9a8866f52ce272dbd0edfede8fbc94d"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -143,7 +143,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-f7009dad99e39c476379.js`))) {
+  if (!resources || !(await caches.match(`/app-905b0ae7eb9f484b531e.js`))) {
     return await fetch(event.request)
   }
 
